7f48d73b8d92a4c5d5cf3d2838c88e368f470f75
Alex Shelton <tbfunkymonkey@gmail.com>
2016-08-29 11:35:24 -0400

    added name to bikes table

--- db/migrate/20160824134200_create_bikes.rb
+++ db/migrate/20160824134200_create_bikes.rb
@@ -1,6 +1,7 @@
 class CreateBikes < ActiveRecord::Migration[5.0]
   def change
     create_table :bikes do |t|
+      t.string :name
       t.string :serial_number
       t.string :model
       t.string :brand
