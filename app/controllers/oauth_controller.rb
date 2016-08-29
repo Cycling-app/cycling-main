e34e4c62bfef53ce1b48458be1a5fe9548d87323
Alex Shelton <tbfunkymonkey@gmail.com>
2016-08-29 17:31:31 -0400

    handled data structure from strava

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -3,6 +3,16 @@ class OauthController < ApplicationController
   def create
     @oauth = StravaOauth.new(params[:code])
 
-    render json: { token: @oauth.get_strava_token }
+    client_info = @oauth.get_strava_token
+
+    @client = Client.create(
+      tokens: client_info["access_token"],
+      first_name: client_info["athlete"]["firstname"],
+      last_name: client_info["athlete"]["lastname"],
+      email: client_info["athlete"]["email"]
+    )
+
+    client_info["client"] = @client
+    render json: client_info
   end
 end
