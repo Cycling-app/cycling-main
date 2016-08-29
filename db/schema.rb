bf9970d4eb99523189ab459f8116270f42b693c9
Alex Shelton <tbfunkymonkey@gmail.com>
2016-08-29 11:30:37 -0400

    changed database to postgres, and updated gemfile for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160824142508) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bikes", force: :cascade do |t|
     t.string   "serial_number"
     t.string   "model"
